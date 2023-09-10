e2e13c40fa8562069d26be812db6c2d71ad36d98
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-11 00:51:05 +0200

    add product list, detail templates with data

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import ProductList, ProductDetail
 
+
+app_name='products'
+
+
 urlpatterns = [
     path('',ProductList.as_view(), name='product_list'),
     path('<int:pk>',ProductDetail.as_view(), name='product_detail'),
