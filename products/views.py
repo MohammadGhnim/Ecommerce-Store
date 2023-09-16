2319257c74f53ddb4a7d4bcc802084a08f394c76
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-16 23:08:55 +0200

    add brand detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from .models import Product, ProductImages, Review, Brand
@@ -14,5 +16,17 @@ class ProductDetail(generic.DetailView):
 class BrandList(generic.ListView):
     model=Brand
 
-class BrandDetail(generic.DetailView):
-    model=Brand
+class BrandDetail(generic.ListView):
+    model=Product
+    template_name='product/brand_detail.html'
+
+    def get_queryset(self):
+        brand=Brand.objects.get(slug=self.kwargs['slug'])
+        queryset=Product.objects.filter(brand=brand)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        return context
+    
