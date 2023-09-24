76db0424f0c0f9284af8e886d2408cd3d938fcf2
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-24 12:41:46 +0200

    add basic signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,11 @@
-from django import forms
-from django.contrib.auth.models import User 
+from django import forms 
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class SignupForm():
+class SignupForm(UserCreationForm):
     class Meta:
-        Model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = User
+        fields = ['username','email','password1','password2']
 
 
 class ActivateUser(forms.Form):
