9fc3357397343046354511297e946dd35ec1d2c1
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-26 15:53:29 +0200

    add utils with generate code, add add code column in profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user=models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     image=models.ImageField(upload_to='accounts')
-
+    code=models.CharField(max_length=10, default=generate_code)
 
     def __str__(self):
         return str(self.user)
