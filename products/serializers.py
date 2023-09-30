2407d8451e577f775d9a9a04cad4da87a5197c22
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-30 23:06:15 +0200

    show avg_reviews in product api serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from .models import Product, Brand
+from django.db.models.aggregates import Avg
 
 
 
 class ProductSerializer(serializers.ModelSerializer):
     brand=serializers.StringRelatedField()
     review_count=serializers.SerializerMethodField()
+    avg_rate=serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields='__all__'
@@ -13,6 +16,17 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_review_count(self, object):
         reviews= object.product_review.all().count()
         return reviews
+    
+
+    def get_avg_rate(self, object):
+        avg = object.product_review.aggregate(avg=Avg('rate'))
+        # if not avg['avg']:
+        #     result=0
+        # else:
+        #     result=round(avg['avg'], 2)
+
+        result=0 if not avg['avg'] else round(avg['avg'], 2)
+        return result
 
 
 class BrandSerializer(serializers.ModelSerializer):
