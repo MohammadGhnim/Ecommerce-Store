93a9a5550fb9089faef654d9ff9c64ffd18ecc76
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-09-30 22:16:48 +0200

    show related in serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,7 @@ from .models import Product, Brand
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand=serializers.StringRelatedField()
     class Meta:
         model = Product
         fields='__all__'
